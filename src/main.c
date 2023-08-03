84ca91e75a63afdb37b37e65985fe8ba1f96e25f
8dcc <8dcc.git@gmail.com>
2023-08-03 16:08:05 +0200

    Add main offset finder

--- src/main.c
+++ src/main.c
@@ -1,13 +1,146 @@
 
 #include <stdbool.h>
+#include <ctype.h>
 #include <stdio.h>
+#include <string.h>
 #include <dlfcn.h>
+#include "sdk.h"
+
+/* Defines that should change depending on the game */
+#define CLIENT_SO "./bms/bin/client.so"
+#define LOG_PATH  "/tmp/source-offset-finder.log"
+
+#define PRINT_TO_FILE(...)        \
+    fprintf(stdout, __VA_ARGS__); \
+    fprintf(log_fd, __VA_ARGS__);
+
+/*----------------------------------------------------------------------------*/
 
 static bool loaded = false;
 
+static FILE* log_fd = 0;
+
+static void* h_client       = NULL; /* client.so handler */
+static BaseClient* i_client = NULL; /* IBaseClientDLL* */
+
+/*----------------------------------------------------------------------------*/
+
+/* Called from globals_init for getting IBaseClientDLL interface */
+void* get_interface(void* handle, const char* name) {
+    typedef void* (*fn)(const char*, int*);
+    static fn CreateInterface = NULL;
+
+    if (!handle) {
+        fprintf(stderr, "get_interface: invalid handle for interface %s\n",
+                name);
+        return NULL;
+    }
+
+    /* Initialize once */
+    if (!CreateInterface) {
+        CreateInterface = (fn)dlsym(handle, "CreateInterface");
+
+        /* dlsym failed */
+        if (!CreateInterface) {
+            fprintf(stderr, "get_interface: dlsym couldn't get "
+                            "CreateInterface\n");
+            return NULL;
+        }
+    }
+
+    return CreateInterface(name, NULL);
+}
+
+/* Called from load (entry point) */
+static bool globals_init(void) {
+    /* Handlers */
+    h_client = dlopen(CLIENT_SO, RTLD_LAZY | RTLD_NOLOAD);
+    if (!h_client) {
+        fprintf(stderr, "globals_init: can't open client.so\n");
+        return false;
+    }
+
+    /* Interfaces */
+    i_client = (BaseClient*)get_interface(h_client, "VClient018");
+    if (!i_client || !i_client->vt) {
+        fprintf(stderr, "globals_init: couldn't load i_client\n");
+        return false;
+    }
+
+    return true;
+}
+
+/* Called from netvars_init and from itself (recursive) */
+static void dump_client_class(const char* network_name, RecvTable* table,
+                              uint32_t offset) {
+    for (int i = 0; i < table->propsCount; i++) {
+        /* Store address of prop at index i in the table->props array */
+        const RecvProp* prop = &table->props[i];
+
+        if (!prop || !prop->varName || isdigit(prop->varName[0]))
+            continue;
+
+        /* We don't care about base classes */
+        if (!strcmp(prop->varName, "baseclass"))
+            continue;
+
+        /* If this is a datatable, there are more props so we dump again
+         * recursively with the new offset */
+        if (prop->recvType == DATATABLE && prop->dataTable &&
+            prop->dataTable->tableName[0] == 'D')
+            dump_client_class(network_name, prop->dataTable,
+                              offset + prop->offset);
+
+        /* Print our shit to stdout and file */
+        PRINT_TO_FILE("%s->%s : 0x%X\n", network_name, prop->varName,
+                      offset + prop->offset);
+    }
+}
+
+/* Called from load (entry point) */
+static void netvars_dump(void) {
+    /* Iterate ClientClass linked list */
+    for (ClientClass* client_class  = i_client->vt->GetAllClasses(i_client);
+         client_class; client_class = client_class->next)
+        /* Make sure it has a valid RecvTable pointer */
+        if (client_class)
+            /* Starting offset is 0, will change with recursion */
+            dump_client_class(client_class->networkName,
+                              client_class->recvTable, 0);
+}
+
+/*----------------------------------------------------------------------------*/
+
+void self_unload(void) {
+    void* self = dlopen("liboffsetfinder.so", RTLD_LAZY | RTLD_NOLOAD);
+
+    /* Close the call we just made to dlopen() */
+    dlclose(self);
+
+    /* Close the call our injector made */
+    dlclose(self);
+}
+
 __attribute__((constructor)) /* Entry point when injected */
 void load(void) {
-    printf("source-offset-finder injected!\n");
+    /* Get file descriptor for /tmp/source-offset-finder.log */
+    log_fd = fopen(LOG_PATH, "w+");
+
+    PRINT_TO_FILE("======================================================\n"
+                  "https://github.com/8dcc/source-offset-finder\n\n"
+                  "Dumping offsets for \"" CLIENT_SO "\"\n"
+                  "======================================================\n\n");
+
+    if (!globals_init()) {
+        fprintf(stderr, "load: error loading globals, aborting\n");
+        self_unload();
+    }
+
+    netvars_dump();
+
+    puts("[source-offset-finder] All done! Check " LOG_PATH);
+
+    fclose(log_fd);
 
     loaded = true;
 }
@@ -18,14 +151,4 @@ void unload() {
         return;
 
     printf("source-offset-finder unloaded.\n\n");
-}
-
-void self_unload(void) {
-    void* self = dlopen("liboffsetfinder.so", RTLD_LAZY | RTLD_NOLOAD);
-
-    /* Close the call we just made to dlopen() */
-    dlclose(self);
-
-    /* Close the call our injector made */
-    dlclose(self);
 }
